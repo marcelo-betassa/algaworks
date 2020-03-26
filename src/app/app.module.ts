8aae30cf2d4418e3c3e1de6152c823e73c12cb54
marcelo-betassa <mhbeta@gmail.com>
2020-03-26 19:37:00 -0300

    refatoracao app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
 import { AppComponent } from "./app.component";
 import { FormsModule } from "@angular/forms";
 import { LancamentosModule } from "./lancamentos/lancamentos.module";
