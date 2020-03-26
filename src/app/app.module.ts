f9c437976949e91d485b779106f3e4f3c4d76b79
marcelo-betassa <mhbeta@gmail.com>
2020-03-26 19:32:42 -0300

    navbar e coremodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,24 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-import { NavbarComponent } from "./navbar/navbar.component";
+import { NavbarComponent } from "./core/navbar/navbar.component";
 import { FormsModule } from "@angular/forms";
 import { LancamentosModule } from "./lancamentos/lancamentos.module";
 import { PessoasModule } from "./pessoas/pessoas.module";
 import { NgxCurrencyModule } from "ngx-currency";
 import { SharedModule } from "./shared/shared.module";
+import { CoreModule } from './core/core.module';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    CoreModule,
     FormsModule,
     PessoasModule,
     LancamentosModule,
