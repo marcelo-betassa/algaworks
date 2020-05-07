e74a0798114a4235a5898a200b8a25336c9f63d8
marcelo-betassa <mhbeta@gmail.com>
2020-05-07 13:34:40 -0300

    enviroment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiURL: "https://algamoney-api.herokuapp.com", // https://algamoney-api.herokuapp.com
-  whitelistedDomains: "https://algamoney-ui-angular.herokuapp.com",
-  blacklistedRoutes:  "https://algamoney-ui-angular.herokuapp.com/oauth/token"
+  whitelistedDomains: "https://algamoney-api.herokuapp.com",
+  blacklistedRoutes:  "https://algamoney-api.herokuapp.com/oauth/token"
 };
