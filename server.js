76690bf5c0f8ee109679b8b224a88aa0dabd39a6
marcelo-betassa <mhbeta@gmail.com>
2020-06-11 07:28:09 -0300

    serverjs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+let server = require('http').Server(app);
 
 app.use(express.static(__dirname + "/dist/algamoney-ui"));
 
@@ -7,6 +8,10 @@ app.get("/*", function(req, resp){
   resp.sendFile(__dirname + "/dist/algamoney-ui/index.html");
 });
 
-app.listen(process.env.PORT || 4200);
+const port = process.env.PORT || 4200;
+server.listen(port, () => {
+  console.log("App is running on port " + port);
+});
+
 
 
